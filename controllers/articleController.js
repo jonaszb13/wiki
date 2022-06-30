352de0056fd204e305c897a83f00a61d323a2403
jonaszb13 <jonas.mueller.jm@gmx.de>
2022-06-30 15:13:10 +0200

    30.06

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,4 +1,5 @@
 const Article = require('../models/article');
+const Old = require('../models/old')
 
 const article_index = (req, res) => {
   Article.find().sort({ createdAt: +1 })
@@ -17,11 +18,14 @@ const article_details = (req, res) => {
   Article.findById(id)
     .then(result => {
       test = result;
-      Article.find().sort({ createdAt: +1})
+      const users = Old.find({ old_id: id})
+      .then(result => {
+        oldID = result;
+        Article.find().sort({ createdAt: +1})
         .then(result => {
-          res.render('details', {articles: result, article: test, title: 'Article Details' })
+          res.render('details', {articles: result, olds: oldID, article: test, title: 'Article Details' })
+      })
     })
-      //res.render('details', {article: result, title: 'Article Details' });
     })
     .catch(err => {
       console.log(err);
@@ -51,6 +55,47 @@ const article_create_post = (req, res) => {
     });
 }
 
+const article_edit_get = (req, res) => {
+  const id = req.params.id;
+      Article.findById(id)
+      .then(result => {
+        test = result;
+        Article.find().sort({ createdAt: +1})
+          .then(result => {
+            res.render('edit', {articles: result, article: test, title: 'Artikel bearbeiten' })
+      })
+      })
+      .catch(err => {
+        console.log(err);
+        res.render('404', { title: 'Article not found' });
+      });
+}
+
+const article_edit_post = (req, res) => {
+  const id = req.params.id;
+  Article.findById(id)
+  .then(result => {
+    const old = new Old(
+      { old_id: result._id,
+        title: result.title, 
+        keywords: result.keywords, 
+        snippet: result.snippet, 
+        body: result.body 
+      })
+    old.save()
+    .then(result => {
+      const id = req.params.id;
+      Article.findByIdAndUpdate(id, req.body)
+        .then(result => {
+          res.redirect('/articles');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+  })
+}
+
 const article_delete = (req, res) => {
   const id = req.params.id;
   Article.findByIdAndDelete(id)
@@ -62,10 +107,18 @@ const article_delete = (req, res) => {
     });
 }
 
+const test_evn = (req, res) => {
+  res.render('test')
+
+}
+
 module.exports = {
   article_index, 
   article_details, 
   article_create_get, 
   article_create_post, 
-  article_delete
+  article_delete,
+  article_edit_get,
+  article_edit_post,
+  test_evn,
 }
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -4,9 +4,12 @@ const articleController = require('../controllers/articleController');
 const router = express.Router();
 
 router.get('/create', articleController.article_create_get);
+router.get('/edit/:id', articleController.article_edit_get);
+router.get('/test', articleController.test_evn);
 router.get('/', articleController.article_index);
 router.post('/', articleController.article_create_post);
 router.get('/:id', articleController.article_details);
 router.delete('/:id', articleController.article_delete);
+router.post('/:id', articleController.article_edit_post);
 
 module.exports = router;
